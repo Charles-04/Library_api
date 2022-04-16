c35fea919fc42cb495756ea123df419496a1cecb
Charles-04 <charlesmir04@gmail.com>
2022-04-16 11:56:51 +0100

    added book model

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersController } from './users/users.controller';
-import { UsersService } from './users/users.service';
+import { UsersController } from './api/users/users.controller';
+import { UsersService } from './api/users/users.service';
+import { BooksController } from './api/books/books.controller';
+import { BooksService } from './api/books/books.service';
 @Module({
   imports: [],
-  controllers: [AppController, UsersController],
-  providers: [AppService, UsersService],
+  controllers: [AppController, UsersController,BooksController],
+  providers: [AppService, UsersService,BooksService],
 })
 export class AppModule {}
