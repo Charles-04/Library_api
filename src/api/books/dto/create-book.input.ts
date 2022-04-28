3332fd4f15c0a6f386d20e6d9af4d5cc9aa2eeec
Charles-04 <charlesmir04@gmail.com>
2022-04-28 15:01:29 +0100

    updated

--- src/api/books/dto/create-book.input.ts
+++ src/api/books/dto/create-book.input.ts
@@ -1,13 +1,15 @@
 import { InputType, Int, Field } from '@nestjs/graphql';
 import { isNotEmpty, IsNotEmpty, IsString } from 'class-validator';
-export class CreateClubInput {
+export class CreateBookInput {
     @Field(() => String, {
       description: 'book name',
     })
     @IsNotEmpty()
     name: string;
 
-    @Field(() => String, { nullable: false, description: 'book Author' })
+    @Field(() => String, { nullable: false,
+         description: 'book Author' 
+        })
     level: string;
 
     @Field(()=>String,{
@@ -15,4 +17,21 @@ export class CreateClubInput {
     })
     @IsNotEmpty()
     code: string;
+
+    @Field(()=>Number,{
+        description:"international serial book number",
+    })
+    @IsNotEmpty()
+    isbn: number;
+
+    @Field(()=>String,{
+        description:"book cover url",
+    })
+    coverImageUrl: string;
+
+    @Field(()=>Number,{
+        description:"book quantity",
+    })
+    @IsNotEmpty()
+    quantity: number;
 }
